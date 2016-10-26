f7befd57d2008d01bb500bb6cc6153e4287f907f
Mattia <m.contin@eggonemail.com>
2016-10-26 15:18:05 +0200

    es 1

---
+++ esercizi_26-10-16/es1.c
@@ -0,0 +1,13 @@
+// Esercizio 1
+// Dato un array con N elementi di tipo Integer, stamparli a video, con indicazione sulla posizione del singolo elemento.
+// es.: [0] = 25
+#include <stdio.h>
+
+int main() {
+    int values[] = {1, 2, 3, 4, 5, 6, 7};
+    int size = sizeof(values)/sizeof(values[0]);
+
+    for(int i = 0; i < size; ++i) {
+        printf("[%d] = %d\n", i, values[i]);
+    }
+}
