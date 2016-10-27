6d219dcfe2c265383e0dba9c9dedb30a7ae07ae5
Mattia <m.contin@eggonemail.com>
2016-10-28 01:00:54 +0200

    completato 9

--- esercizi_26-10-16/es9.c
+++ esercizi_26-10-16/es9.c
@@ -1,34 +1,80 @@
 #include <stdio.h>
 
-void quick_sort(float values[], int size) {
-
-    printf("%d\n", size);
+void quick_sort_asc(float values[], int size) {
+    if(size <= 1) {
+        return;
+    }
 
     int pivotIndex = 0;
-    float pivot = values[pivotIndex];
-    int lastBigger = 0;
+    int indexToSwap = 1;
+    float swapper;
 
     for(int i = 1; i < size; ++i) {
-        if(values[i] < pivot) {
-            float temp = values[i];
-            values[i] = values[lastBigger];
-            values[lastBigger] = temp;
-            lastBigger += 1;
+        if(values[i] < values[pivotIndex]) {
+            swapper = values[i];
+            values[i] = values[indexToSwap];
+            values[indexToSwap] = swapper;
+            ++indexToSwap;
         }
     }
 
-    for(int i = 0; i < size; ++i) {
-        printf("%f\n", values[i]);
+    swapper = values[pivotIndex];
+    values[pivotIndex] = values[indexToSwap - 1];
+    values[indexToSwap - 1] = swapper;
+
+    // left side
+    quick_sort_asc(values, pivotIndex);
+    // right side
+    quick_sort_asc(values + pivotIndex + 1, size - 1 - pivotIndex);
+}
+
+void quick_sort_desc(float values[], int size) {
+    if(size <= 1) {
+        return;
     }
 
+    int pivotIndex = 0;
+    int indexToSwap = 1;
+    float swapper;
+
+    for(int i = 1; i < size; ++i) {
+        if(values[i] > values[pivotIndex]) {
+            swapper = values[i];
+            values[i] = values[indexToSwap];
+            values[indexToSwap] = swapper;
+            ++indexToSwap;
+        }
+    }
+
+    swapper = values[pivotIndex];
+    values[pivotIndex] = values[indexToSwap - 1];
+    values[indexToSwap - 1] = swapper;
+
+    // left side
+    quick_sort_desc(values, pivotIndex);
+    // right side
+    quick_sort_desc(values + pivotIndex + 1, size - 1 - pivotIndex);
 }
 
 //Esercizio 9
 //Dato il seguente array { 35.4, 46.7, 77.55, 11.1, 9.04, 0.75 } di elementi di tipo float,
 //eseguire l’ordinamento crescente e decrescente, stamparne il risultato a video
 void main() {
-    float values[] = {35.4, 46.7, 77.55, 11.1, 9.04, 0.75};
+    float values[] = {35.4, 46.7, 77.55, 11.1, 9.04, 0.75, 32.1, 64.7, 12.6, 75.1, 765.24, 0.432};
     int size = sizeof(values)/sizeof(values[0]);
 
-    quick_sort(values, size);
+    quick_sort_asc(values, size);
+
+    for(int i = 0; i < size; ++i) {
+        printf("%.2f ", values[i]);
+    }
+
+    printf("\n");
+
+    quick_sort_desc(values, size);
+
+    for(int i = 0; i < size; ++i) {
+        printf("%.2f ", values[i]);
+    }
+
 }
